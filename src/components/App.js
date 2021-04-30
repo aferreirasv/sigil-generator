77bb70850abc5319370163ea340f6961b060ee80
Alan Ferreira <aferreirasv@gmail.com>
2021-04-30 14:17:28 -0300

    Corrige bug de camadas

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,28 @@
-import React, { useEffect, useMemo, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Button } from "@material-ui/core"
 import { makeStyles } from "@material-ui/styles"
 import quadrado from '../assets/quadrado.png'
 import circulo from '../assets/circulo.png'
 import flor from '../assets/flor.png'
 import fogo from '../assets/fogo.png'
+import raizes from '../assets/raizes.png'
+import chamas from '../assets/chamas.png'
 
 
 const images = [
-  [circulo, quadrado],
-  [fogo, flor],
-  [fogo, flor]
+  [{ src: circulo, name: "circulo" }, { src: quadrado, name: "quadrado" }],
+  [{ src: fogo, name: "fogo" }, { src: flor, name: "flor" }],
+  [{ src: chamas, name: "chamas" }, { src: raizes, name: "raizes" }]
+
 ]
 
 const types = {
   BACKGROUND: 0,
   ELEMENT: 1,
-  OTHER: 2
+  ELEMENT2: 2,
+  OTHER: 3
 }
+
 
 const MENU_OPTIONS = [
   {
@@ -28,33 +33,23 @@ const MENU_OPTIONS = [
     name: "Elemento 1",
     type: types.ELEMENT,
   },
-  // {
-  //   name: "Elemento 2",
-  //   type: types.ELEMENT,
-  // },
-  // {
-  //   name: "Outros",
-  //   type: types.OTHER,
-  // },
+  {
+    name: "Elemento 2",
+    type: types.ELEMENT2,
+  },
+  {
+    name: "Outros",
+    type: types.OTHER,
+  },
 ]
 
 function App() {
   const [selectedType, selectType] = useState(0)
+
   const [layers, setLayers] = useState(MENU_OPTIONS.map(_ => null))
-  // const images = [
-  //   { type: "circle", src: circulo },
-  //   { type: "circle", src: quadrado },
-  //   { type: "element", src: fogo },
-  //   { type: "element", src: flor },
-  //   { type: "element2", src: fogo },
-  //   { type: "element2", src: flor },
-  // ]
-  // const images = {
-  //   circle: [circulo, quadrado],
-  //   element: [fogo, flor],
-  //   element2: [fogo, flor]
-  // }
-  
+
+
+
 
   const useStyles = makeStyles({
     canvas: {
@@ -91,29 +86,30 @@ function App() {
   })
   const classes = useStyles()
 
-  const handleCircle = (e) => {
-    selectType("circle")
-  }
-  const handleElement = (e) => {
-    selectType("element")
-  }
-  const handleElement2 = (e) => {
-    selectType("element2")
-  }
-  const handleOthers = (e) => {
-    selectType("others")
-  }
-
 
   const handleImageClick = (e) => {
     // console.log(e.target)
-    console.log({selectedType})
-    const newLayers = layers.map((l, i) => selectedType == i ? e.target : l)
-    console.log({newLayers})
+    const newLayers = layers.map((l, i) => {
+      if (l) console.log("L ALT", l.alt)
+      if (l) console.log("TARGET ALT", e.target.alt)
+      if (l) console.log(l.alt === e.target.alt)
+
+
+      if (l && l.id === e.target.id) {
+        return null
+      }
+      else if (selectedType === i) {
+        return e.target.cloneNode(true)
+      } else {
+        return l
+      }
+    }
+    )
+    console.log({ newLayers })
     setLayers(newLayers)
-        // setLayers(layers.map(v => v.name === getType(e) ? { name: v.name, image: e.target } : v))
+    // setLayers(layers.map(v => v.name === getType(e) ? { name: v.name, image: e.target } : v))
   }
-  
+
   const handleExport = (e) => {
     // let canvas = document.getElementById("canvas-circle")
     // let image = canvas.toDataURL("image/png")
@@ -132,10 +128,9 @@ function App() {
   function drawLayers() {
     console.log("Drawing Layers")
     let canvas = document.getElementById("canvas")
-    console.log("canvas", canvas)
     let ctx = canvas.getContext("2d")
     ctx.clearRect(0, 0, canvas.width, canvas.height)
-    console.log({layers})
+    // console.log({ layers })
     layers.forEach(layer => layer && ctx.drawImage(layer, 0, 0))
   }
 
@@ -146,7 +141,7 @@ function App() {
     <div className={classes.app}>
       <div className={classes.buttons}>
         {
-          MENU_OPTIONS.map(opt => 
+          MENU_OPTIONS.map(opt =>
             <Button onClick={_ => selectType(opt.type)}>{opt.name}</Button>)
         }
         <Button onClick={handleExport}>Export</Button>
@@ -154,8 +149,9 @@ function App() {
       <div className={classes.images}>
         {
           images[selectedType]
-              .map(v => 
-                <img src={v} alt={`sigil-img`} className={classes.image} onClick={handleImageClick} />)
+            .map(v => {
+              return <img src={v.src} alt={`sigil-img`} id={`${v.name}${v.layer}`} className={classes.image} onClick={handleImageClick} />
+            })
         }
       </div>
       <canvas className={classes.canvas} id="canvas" width="800" height="800" />
